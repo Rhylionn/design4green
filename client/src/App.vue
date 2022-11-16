db2d75fb4a9a193487fe03457d9652989c6eb363
BethGarion <tperrault.ing2024@esaip.org>
2022-11-16 16:22:13 +0100

    Ajout Footer and Header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,22 @@
 <template>
-<h1 class="text-4xl text-red-600">Salut a tous</h1>
+    <div>
+        <Header />
+            <h1 class="text-4xl text-red-600">Salut a tous</h1>
+        <FooterInformation />
+    </div>
 </template>
+
+
+<script>
+import FooterInformation from "./components/FooterInformation.vue"
+import Header from "./components/HeaderInformation.vue"
+export default {
+  components: {
+    FooterInformation,
+    Header
+  },
+  setup() {
+
+  },
+}
+</script>
---
+++ client/src/components/HeaderInformation.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="text-center p-3 bg-cyan-900 h-20 flex justify-between items-center">
+    <!---->
+    <p class="text-white">
+      -=[ ARTI'CHAUD ]=-
+    </p>
+    <h1 class="text-white text-4xl">Choix des formations</h1>
+    <button
+      class="h-8 w-44 top-12 rounded-lg bg-white text-black text-2xl flex items-center justify-center"
+    >
+      Panier
+    </button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
