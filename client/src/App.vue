4549f18966e040cfb39315f7e895544e3276c09e
HugoDELCROIX <hugo.delcroix@hotmail.com>
2022-11-17 11:05:48 +0100

    la pression, je la bois

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,37 +1,39 @@
-<template></template>
-
-<script>
-import { ref, onBeforeMount } from "vue"
-
-export default {
-  name: "App",
-  setup() {
-    const formations = ref([])
-
-    onBeforeMount(async () => {
-      formations.value = await getFormations()
-    })
-
-    // Récupère les formations soit par le localStorage soit en fetchant l’API
-    async function getFormations() {
-      let formations
-      if (localStorage.getItem("formations") === null) {
-        formations = await fetchFormations()
-        localStorage.setItem("formations", JSON.stringify(formations))
-      } else {
-        formations = JSON.parse(localStorage.getItem("formations"))
+<template>
+    <RouterView :formations="formations" />
+  </template>
+  
+  <script>
+  import { ref, onBeforeMount } from "vue";
+  export default {
+    name: "App",
+    setup() {
+      const formations = ref([]);
+      onBeforeMount(async () => {
+        formations.value = await getFormations();
+      });
+      async function getFormations() {
+        let formationsRequest
+        if (localStorage.getItem("formations") === null) {
+          formationsRequest = await fetchFormations()
+          localStorage.setItem("formations", JSON.stringify(formationsRequest))
+        } else {
+          formationsRequest = JSON.parse(localStorage.getItem("formations"))
+        }
+        return formationsRequest
       }
-
-      return formations
-    }
-
-    // Fonction séparée pour fetch les formations
-    async function fetchFormations() {
-      const endpoint = `${import.meta.env.VITE_API_URL}/formations`
-
-      const response = await fetch(endpoint)
-      return response.json()
-    }
-  },
-}
-</script>
+      
+      // Fonction séparée pour fetch les formations
+      async function fetchFormations() {
+        const endpoint = `${import.meta.env.VITE_API_URL}/formations`;
+        const response = await fetch(endpoint);
+        return response.json();
+      }
+      onBeforeMount(async () => {
+        formations.value = await getFormations()
+      })
+      return { formations }
+    },
+  };
+  </script>
+  Footer
+  
