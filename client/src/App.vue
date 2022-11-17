e80de9595607f8c6c9865b2292a3daebdcea1709
BethGarion <tperrault.ing2024@esaip.org>
2022-11-17 03:32:19 +0100

    Ajout Modal

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,37 +1,54 @@
-<template></template>
+<template>
+  <div>
+    <HeaderInformation />
+
+    <body>
+      <p></p>
+    </body>
+
+    <FooterInformation />
+  </div>
+</template>
 
 <script>
-import { ref, onBeforeMount } from "vue"
+import { ref, onBeforeMount } from "vue";
+import FooterInformation from "./components/FooterInformation.vue";
+import HeaderInformation from "./components/HeaderInformation.vue";
+//import BaseModal from "./components/BaseModal.vue";
 
 export default {
   name: "App",
+  components: {
+    FooterInformation,
+    HeaderInformation,
+    //BaseModal,
+  },
+
   setup() {
-    const formations = ref([])
+    const formations = ref([]);
 
     onBeforeMount(async () => {
-      formations.value = await getFormations()
-    })
+      formations.value = await getFormations();
+    });
 
     // Récupère les formations soit par le localStorage soit en fetchant l’API
     async function getFormations() {
-      let formations
+      let formations;
       if (localStorage.getItem("formations") === null) {
-        formations = await fetchFormations()
-        localStorage.setItem("formations", JSON.stringify(formations))
+        formations = await fetchFormations();
+        localStorage.setItem("formations", JSON.stringify(formations));
       } else {
-        formations = JSON.parse(localStorage.getItem("formations"))
+        formations = JSON.parse(localStorage.getItem("formations"));
       }
-
-      return formations
+      return formations;
     }
-
     // Fonction séparée pour fetch les formations
     async function fetchFormations() {
-      const endpoint = `${import.meta.env.VITE_API_URL}/formations`
+      const endpoint = `${import.meta.env.VITE_API_URL}/formations`;
 
-      const response = await fetch(endpoint)
-      return response.json()
+      const response = await fetch(endpoint);
+      return response.json();
     }
   },
-}
+};
 </script>
---
+++ client/src/components/FooterInformation.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class="text-center p-3 bg-cyan-900 text-white">
+    <div>
+        <p>[This is the Footer]</p>
+    </div>
+    </footer>
+</template>
+    
+<script>
+export default {
+  name: "FooterInformation",
+}
+</script>
