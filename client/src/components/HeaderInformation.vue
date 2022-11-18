26284313bb6d3c6c23c8f9d9829976083e5e3e60
Rhylionn <tbernard.p@protonmail.com>
2022-11-18 02:42:47 +0100

    Merge pull request #39 from Rhylionn/report working report + accessibility improved

--- client/src/components/HeaderInformation.vue
+++ client/src/components/HeaderInformation.vue
@@ -12,42 +12,103 @@
       class="w-fit px-3 py-3 sm:px-5 sm:py-2 rounded-xl bg-white text-black text-2xl flex items-center justify-center cursor-pointer"
       @click="toggleModal"
     >
-      <FontAwesomeIcon
-				alt=""
-				icon="fa-cart-shopping"
-			/>
-			<span class="md:ml-2 hidden sm:flex text-sm">mon panier</span>
+      <FontAwesomeIcon alt="" icon="fa-cart-shopping" />
+      <span class="md:ml-2 hidden sm:flex text-sm">mon panier</span>
     </button>
- 
+
     <CartModal :modalActive="modalActive" @close-modal="closeModal">
-      <div class="text-xs sm:text-sm md:text-base flex justify-between items-center w-4/5">
-        <a v-show="!cartContent()" :href="sendMail()" class="text-white mt-8 h-16 sm:h-14 md:h-16 p-2 w-1/3 bg-space-cadet rounded-lg cursor-pointer hover:bg-black-coral">Demander un devis par mail</a>
-        <button v-show="!cartContent()" class="text-white mt-8 h-16 sm:h-14 md:h-16 p-2 w-1/3 bg-space-cadet rounded-lg cursor-pointer hover:bg-black-coral">Exporter en PDF</button>
+      <div
+        class="text-xs sm:text-sm md:text-base flex justify-between items-center w-4/5"
+      >
+        <a
+          v-show="!cartContent()"
+          :href="sendMail()"
+          class="text-white mt-8 h-16 sm:h-14 md:h-16 p-2 w-1/3 bg-space-cadet rounded-lg cursor-pointer hover:bg-black-coral print:hidden"
+          >Demander un devis par mail</a
+        >
+        <button
+          v-show="!cartContent()"
+          @click="generateReport()"
+          class="text-white mt-8 h-16 sm:h-14 md:h-16 p-2 w-1/3 bg-space-cadet rounded-lg cursor-pointer hover:bg-black-coral print:hidden"
+        >
+          Exporter en PDF
+        </button>
       </div>
-        <p class="mt-5" v-show="cartContent()">Votre panier est vide !</p>
-				<h1 class="hidden print:block text-2xl mb-10">Récapitulatif des formations sélectionnées</h1>
-        <table v-if="!cartContent()" class="m-2 text-xs md:text-base w-4/5 table-auto" media="print">
-          <thead class="p-2 border-2 border-space-cadet ">
-            <tr>
-              <th class="p-2 border-2 border-space-cadet ">Intitulé</th>
-              <th class="p-2 border-2 border-space-cadet hidden print:table-cell md:table-cell">Type</th>
-              <th class="p-2 border-2 border-space-cadet hidden print:table-cell md:table-cell">Structure</th>
-              <th class="p-2 border-2 border-space-cadet hidden print:table-cell md:table-cell">Durée</th>
-              <th class="p-2 border-2 border-space-cadet hidden print:table-cell lg:table-cell">Emplacement</th>
-              <th class="p-2 border-2 print:hidden border-space-cadet ">Supprimer la formation</th>
-            </tr>
-          </thead>
-          <tbody :key="index" v-for="(formation, index) in cart" class="p-2 border-2 border-space-cadet text-center">
-            <tr>
-              <td class="p-2 border-2 border-space-cadet ">{{formation.formationName}}</td>
-              <td class="p-2 border-2 border-space-cadet hidden print:table-cell md:table-cell">{{formation.typeFormation}}</td>
-              <td class="p-2 border-2 border-space-cadet hidden print:table-cell md:table-cell">{{formation.Structure}}</td>
-              <td class="p-2 border-2 border-space-cadet hidden print:table-cell md:table-cell">{{formation.duration}}</td>
-              <td class="p-2 border-2 border-space-cadet hidden print:table-cell lg:table-cell">{{formation.structLoc}}</td>
-              <td class="p-2 border-2 print:hidden border-space-cadet hover:bg-orange cursor-pointer" @click="deleteFormation(formation)">Supprimer</td>
-            </tr>
-          </tbody>
-        </table>
+      <p class="mt-5" v-show="cartContent()">Votre panier est vide !</p>
+      <h1 class="hidden print:block text-2xl mb-10">
+        Récapitulatif des formations sélectionnées
+      </h1>
+      <table
+        v-if="!cartContent()"
+        class="m-2 text-xs md:text-base w-4/5 table-auto"
+        media="print"
+      >
+        <thead class="p-2 border-2 border-space-cadet">
+          <tr>
+            <th class="p-2 border-2 border-space-cadet">Intitulé</th>
+            <th
+              class="p-2 border-2 border-space-cadet hidden print:table-cell md:table-cell"
+            >
+              Type
+            </th>
+            <th
+              class="p-2 border-2 border-space-cadet hidden print:table-cell md:table-cell"
+            >
+              Structure
+            </th>
+            <th
+              class="p-2 border-2 border-space-cadet hidden print:table-cell md:table-cell"
+            >
+              Durée
+            </th>
+            <th
+              class="p-2 border-2 border-space-cadet hidden print:table-cell lg:table-cell"
+            >
+              Emplacement
+            </th>
+            <th class="p-2 border-2 print:hidden border-space-cadet">
+              Supprimer la formation
+            </th>
+          </tr>
+        </thead>
+        <tbody
+          :key="index"
+          v-for="(formation, index) in cart"
+          class="p-2 border-2 border-space-cadet text-center"
+        >
+          <tr>
+            <td class="p-2 border-2 border-space-cadet">
+              {{ formation.formationName }}
+            </td>
+            <td
+              class="p-2 border-2 border-space-cadet hidden print:table-cell md:table-cell"
+            >
+              {{ formation.typeFormation }}
+            </td>
+            <td
+              class="p-2 border-2 border-space-cadet hidden print:table-cell md:table-cell"
+            >
+              {{ formation.Structure }}
+            </td>
+            <td
+              class="p-2 border-2 border-space-cadet hidden print:table-cell md:table-cell"
+            >
+              {{ formation.duration }}
+            </td>
+            <td
+              class="p-2 border-2 border-space-cadet hidden print:table-cell lg:table-cell"
+            >
+              {{ formation.structLoc }}
+            </td>
+            <td
+              class="p-2 border-2 print:hidden border-space-cadet hover:bg-orange cursor-pointer"
+              @click="deleteFormation(formation)"
+            >
+              Supprimer
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </CartModal>
   </header>
 </template>
@@ -84,25 +145,37 @@ export default {
     function cartContent() {
       return cart.length == 0 ? true : false
     }
-		
-		function generateReport () {
-			window.print()
-		}
+
+    function generateReport() {
+      window.print()
+    }
 
     function deleteFormation(formation) {
       cartStore.removeFormation(formation)
     }
 
-    function sendMail(){
-        const objet = `Demande de devis pour ${cart.length} formation(s)`
-        let message = "Bonjour, \n Je souhaiterai avoir un devis pour les formations suivantes :\n"
-        for(let i=0;i<cart.length;i++){
-          message += `- Formation par ${cart[i].organismeName} d'une durée de ${cart[i].duration} jour(s)\n`
-        }
-        return `mailto:design4green@etik.com?subject=${encodeURIComponent(objet)}&body=${encodeURIComponent(message)}`
+    function sendMail() {
+      const objet = `Demande de devis pour ${cart.length} formation(s)`
+      let message =
+        "Bonjour, \n Je souhaiterai avoir un devis pour les formations suivantes :\n"
+      for (let i = 0; i < cart.length; i++) {
+        message += `- Formation par ${cart[i].organismeName} d'une durée de ${cart[i].duration} jour(s)\n`
+      }
+      return `mailto:design4green@etik.com?subject=${encodeURIComponent(
+        objet
+      )}&body=${encodeURIComponent(message)}`
     }
 
-    return { modalActive, toggleModal, cart, cartContent, deleteFormation, sendMail, closeModal, generateReport}
+    return {
+      modalActive,
+      toggleModal,
+      cart,
+      cartContent,
+      deleteFormation,
+      sendMail,
+      closeModal,
+      generateReport,
+    }
   },
 }
 </script>
