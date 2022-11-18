92a1182b932e3527670a996961f9ed53c45a7401
Rhylionn <tbernard.p@protonmail.com>
2022-11-18 02:16:31 +0100

    added compression

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import viteCompression from "vite-plugin-compression"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {},
-  plugins: [vue()],
+  plugins: [vue(), viteCompression()],
 })
